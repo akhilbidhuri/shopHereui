16d727495c59b75414d8a3b1bf8947ed56b77457
Akhil Bidhuri <akhilbidhuri@gmail.com>
2021-06-13 23:58:10 +0530

    removed redirect(not required)

--- src/EStore/EStore.js
+++ src/EStore/EStore.js
@@ -3,17 +3,12 @@ import TopBar from './components/TopBar'
 import UserContext from '../state/userContext'
 import { getItems } from '../services/service'
 import Catalouge from './components/Catalouge';
-import { Redirect } from 'react-router-dom'
 
 function EStore(props) {
     const { state } = useContext(UserContext);
     const [items, setItems] = useState([])
-    const [redirect, setRedirect] = useState(false)
 
     useEffect(async() => {
-        if(state.logout){
-            setRedirect(true)
-        }
         (async function () {
             let res = await getItems(state.user.token)
             if(res){
@@ -27,9 +22,6 @@ function EStore(props) {
 
     return (
         <>
-            {redirect &&
-                <Redirect to="/"/>
-            }
             <TopBar/>
             <Catalouge items={items}/>
         </>
